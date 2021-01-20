6f489ec901dfe66a1e8af5b32d3afe76e876e2d9
Helba The AI <toyzz@live.com>
2021-01-20 00:03:08 -0800

    added new homepage

--- main.go
+++ main.go
@@ -5,6 +5,25 @@ import (
 	"uwu/uwu"
 )
 
+const html = `
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>UwUify</title>
+<meta name="description" content="UwUify your text for the fun of it!">
+<meta name="author" content="Helba The AI">
+</head>
+<body>
+<h3>UwUify<h3>
+	<strong>https://uwuify.helba.ai/?uwu=<code>Your text here</code><strong>
+	<br />
+	<br />
+	<a href="https://github.com/Toyz/uwu-server" target="_blank">Source code on Github</a>
+</body>
+</html>
+`
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
@@ -15,7 +34,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	keys, ok := r.URL.Query()["uwu"]
 
 	if !ok || len(keys[0]) < 1 {
-		w.Write([]byte("'uwu' param is missing"))
+		w.Header().Set("Content-Type", "text/html")
+
+		w.Write([]byte(html))
 		return
 	}
 
